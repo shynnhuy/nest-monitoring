00d68cf78fc1ee1e8b38d84f38f0d7bbd08459f1
shynnhuy <shynn@hotmail.com>
2023-04-10 23:41:26 +0700

    check disk

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -14,23 +14,22 @@ export class HealthController {
     //   () => this.http.pingCheck('nestjs-docs', 'http://localhost:3000'),
     // ]);
     // console.log(si);
-    const stat = await pidusage(process.pid);
+    // const stat = await pidusage(process.pid);
 
-    let _stat: any = {};
+    // let _stat: any = {};
 
-    _stat = { ...stat };
-    // Convert from B to MB
-    _stat.memory = stat.memory / 1024 / 1024;
-    _stat.load = os.loadavg();
-    _stat.timestamp = Date.now();
+    // _stat = { ...stat };
+    // // Convert from B to MB
+    // _stat.memory = stat.memory / 1024 / 1024;
+    // _stat.load = os.loadavg();
+    // _stat.timestamp = Date.now();
 
-    const space = this.health.getDiskSpace();
-
-    console.log(_stat);
+    // console.log(_stat);
 
     const cpu = await this.health.currentLoad();
     const memory = await this.health.memory();
+    const disks = await this.health.getDiskSpace();
 
-    return { cpu, memory, space };
+    return { cpu, memory, disks };
   }
 }
--- src/health/health.service.ts
+++ src/health/health.service.ts
@@ -1,12 +1,10 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
-import { exec } from 'child_process';
-
 import * as pidusage from 'pidusage';
-import * as os from 'os';
 import * as si from 'systeminformation';
 
+const toGB = (byte: number) => byte / 1024 / 1024 / 1024;
 @Injectable()
 export class HealthService {
   private readonly logger = new Logger(HealthService.name);
@@ -18,15 +16,23 @@ export class HealthService {
     const cpu = await this.currentLoad();
     const memory = await this.memory();
 
-    let _stat: any = {};
+    const _stat: any = {};
 
-    _stat = { ...stat };
+    // _stat = { ...stat };
     // Convert from B to MB
-    _stat.memory = stat.memory / 1024 / 1024;
+    // _stat.memory = stat.memory / 1024 / 1024;
     // _stat.load = os.loadavg();
-    _stat.timestamp = Date.now();
-    _stat.load = cpu;
-    _stat.memory = memory;
+    // _stat.timestamp = Date.now();
+    // _stat.load = cpu;
+    _stat.memory = memory.map((mem) => ({
+      // ...mem,
+      device: mem.device,
+      size: `${mem.size / 1024 / 1024 / 1024}`,
+    }));
+
+    const disks = await this.getDiskSpace();
+
+    _stat.disk = disks;
     this.logger.debug(_stat);
   }
 
@@ -44,27 +50,28 @@ export class HealthService {
   }
   async memory() {
     try {
-      const { free, total, used } = await si.mem();
-      return { free, total, used };
+      const disks = await si.diskLayout();
+      return disks;
     } catch (error) {
       console.log(error);
     }
   }
 
-  getDiskSpace(dir = '/') {
-    const cmd = 'df -h --output=pcent ' + dir;
+  async getDiskSpace(dir = '/') {
+    // const cmd = 'df -h --output=pcent ' + dir;
+    // const cmd = `df -P ${dir} | awk '/%/ {print $5}'`;
 
-    let space = '';
-    exec(cmd, function (err, data) {
-      if (err) {
-        // Error handling
-        console.log('ERROR', err);
-      } else {
-        // Sucess response handling
-        console.log('SUCCESS', data);
-        space = data;
-      }
-    });
-    return space;
+    // const space = execSync(cmd);
+
+    // return space.toString().split('\n')[0];
+
+    const sizes = await si.fsSize();
+    return sizes.map((disk) => ({
+      size: toGB(disk.size),
+      used: toGB(disk.used),
+      available: toGB(disk.available),
+      use: disk.use,
+      mount: disk.mount,
+    }));
   }
 }
